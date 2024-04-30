3ddccb1e9df82acd3c084131895b3cc6cc2e76ec
muzaffar <muzsamgio@gmail.com>
2024-04-30 18:18:39 +0500

    new file:   lib/task.dart

---
+++ lib/task.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Task extends StatefulWidget {
+  const Task({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _TaskState();
+  }
+}
+
+class _TaskState extends State<Task> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
